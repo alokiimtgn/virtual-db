976ccc0b72a15ba7adaf414613c2ae11f7d51b00
Alok Kumar <61052243+alokiimtgn@users.noreply.github.com>
2021-01-11 12:51:43 +0530

    Create testlist.php

---
+++ testlist.php
@@ -0,0 +1,88 @@
+"results"=[
+    
+    
+    {
+            "testID":"Test#01",
+            "heading":"Profit and Loss",
+            "createdBy":"SP Singh",
+            "timestamp":"Monday 19th Aprill 2020 , 08:29pm",
+            "isTestActive":true,
+            "testDescription" : "Contains Basic questions of Profit & Loss",
+            "url":"http://eduthirst.com/mjson/profitloss.json",
+            "audience":"Graduates"
+},
+{
+            "testID":"Test#02",
+            "heading":"Basics of Javascript",
+            "createdBy":"Alok Kumar",
+            "timestamp":"Monday 19th Aprill 2020 , 08:29pm",
+            "isTestActive":true,
+            "testDescription" : "",
+            "url":"http://eduthirst.com/mjson/javascript1.json",
+            "audience":"Graduates"
+},{
+            "testID":"Test#03",
+            "heading":"Basics of Java",
+            "createdBy":"Alok Kumar",
+            "timestamp":"Monday 19th Aprill 2020 , 08:29pm",
+            "isTestActive":true,
+            "testDescription" : "as",
+            "url":"http://eduthirst.com/mjson/java1.json",
+            "audience":"Graduates"
+},{
+            "testID":"Test#04",
+            "heading":"Time and Work",
+            "createdBy":"SP Singh",
+            "timestamp":"Monday 24th May 2020 , 11:38am",
+            "isTestActive":false,
+            "testDescription" : "Contains Basic questions of Time and work",
+            "url":"http://eduthirst.com/mjson/timenwork.json",
+            "audience":"Graduates"
+},{
+            "testID":"Test#05",
+            "heading":"Partnership",
+            "createdBy":"SP Singh",
+            "timestamp":"Monday 24th May 2020 , 11:38am",
+            "isTestActive":false,
+            "testDescription" : "Contains Basic questions of partnership",
+            "url":"http://eduthirst.com/mjson/partnership.json",
+            "audience":"Graduates"
+},
+{
+            "testID":"Test#06",
+            "heading":"Percentage",
+            "createdBy":"SP Singh",
+            "timestamp":"Monday 24th May 2020 , 11:38am",
+            "isTestActive":false,
+            "testDescription" : "Contains Basic questions of Percentage",
+            "url":"http://eduthirst.com/mjson/percentage.json",
+            "audience":"Graduates"
+},{
+            "testID":"Test#06",
+            "heading":"Series & Odd Man out",
+            "createdBy":"SP Singh",
+            "timestamp":"Sunday 24th May 2020 , 11:38am",
+            "isTestActive":true,
+            "testDescription" : "Basic questions of Series & Odd man out",
+            "url":"http://eduthirst.com/mjson/seriesoddman.json",
+            "audience":"Graduates"
+},{
+            "testID":"Test#07",
+            "heading":"Time & Distance",
+            "createdBy":"SP Singh",
+            "timestamp":"Monday 24th May 2020 , 11:38am",
+            "isTestActive":false,
+            "testDescription" : "Basic questions of Time & Distance",
+            "url":"http://eduthirst.com/mjson/timendistance.json",
+            "audience":"Graduates"
+},{
+            "testID":"Test#08",
+            "heading":"Logarithms",
+            "createdBy":"SP Singh",
+            "timestamp":"Monday 24th May 2020 , 11:38am",
+            "isTestActive":false,
+            "testDescription" : "Basic questions of Time & Distance",
+            "url":"http://eduthirst.com/mjson/logarithms.json",
+            "audience":"Graduates"
+}
+]
